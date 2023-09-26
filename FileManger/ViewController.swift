a4f75495b66b218e3a6dca8e6348031c59d16e61
AlexeyManankov <virtbrain@gmail.com>
2023-09-26 19:37:04 +0500

    final commit

--- FileManger/ViewController.swift
+++ FileManger/ViewController.swift
@@ -11,7 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let manager = FileManager.default
+        
+        guard let url = manager.urls(
+            for: .documentDirectory,
+            in: .userDomainMask).first 
+        else {
+            return
+        }
+        //MARK: Append Folder
+        let newFolderUrl = url.appendingPathComponent("SpiderFolder")
+        
+        //MARK: Append File
+        let filePath = newFolderUrl.appendingPathComponent("logs.txt")
+        //MARK: Add file with data
+        let  textString = "Writing text to a text file! Woooo".data(using: .utf8)
+        manager.createFile(
+            atPath: filePath.path,
+            contents: textString
+            )
+            
+        //MARK: Delete file or folder
+//        if manager.fileExists(atPath: filePath.path) {
+//            do {
+//                try manager.removeItem(atPath: filePath.path)
+//            } catch {
+//                print(error)
+//            }
+//        }
+        
+        let data = manager.contents(atPath: filePath.path)
+        print(String(decoding: data ?? Data(), as: UTF8.self))
     }
 
 
